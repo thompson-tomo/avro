c9999387c1110e61967c449b234dc138191dbaca
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2022-10-03 14:52:24 +0300

    AVRO-3631: Add support for ser_de Value::Fixed
    It is based on https://github.com/serde-rs/bytes/pull/28 which is not
    yet merged.
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- lang/rust/avro/src/types.rs
+++ lang/rust/avro/src/types.rs
@@ -1163,6 +1163,7 @@ mod tests {
     use pretty_assertions::assert_eq;
     use serde::{Deserialize, Serialize};
     use serde_json::json;
+    use serde_bytes::ByteArray;
     use uuid::Uuid;
 
     #[test]
@@ -3114,14 +3115,16 @@ Field with name '"b"' is not a member of the map items"#,
         );
     }
 
-    #[derive(Debug, Serialize, Deserialize)]
-    struct TestStructFixedField {
-        field: [u8; 6],
-    }
-
     #[test]
-    fn test_avro_3631_serialize_fixed_fields() {
-        let test = TestStructFixedField { field: [1; 6] };
+    fn avro_3631_test_serialize_fixed_fields() {
+        #[derive(Debug, Serialize, Deserialize)]
+        struct TestStructFixedField {
+            field: ByteArray<6>,
+        }
+
+        let test = TestStructFixedField {
+            field: ByteArray::new([1; 6]),
+        };
         let value: Value = to_value(test).unwrap();
         let schema = Schema::parse_str(
             r#"
